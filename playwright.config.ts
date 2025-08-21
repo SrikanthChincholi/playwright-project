67409005604772a58e53b24c0cf6b7f284b226ba
srikanthchincholi <srikanthchincholi@yahoo.com>
2025-08-21 23:05:33 +0530

    adding a project

---
+++ playwright.config.ts
@@ -0,0 +1,105 @@
+import { defineConfig, devices } from '@playwright/test';
+import { OrtoniReportConfig } from "ortoni-report";
+
+    const reportConfig: OrtoniReportConfig = {
+      open: process.env.CI ? "never" : "always", // Opens report automatically after tests, unless in CI
+      folderPath: "./ortoni-reports", // Folder to store report data
+      filename: "playwright-ortoni.html", // Name of the main HTML report file
+      title: "My Playwright Test Report", // Title of the report
+      showProject: true, // Show project name in the report
+      projectName: "Playwright", // Project name
+      testType: "e2e", // Type of tests (e.g., "e2e", "functional")
+      authorName: "Srikanth Chincholi", // Author name
+      preferredTheme: "dark", // "light" or "dark"
+      meta: { // Optional metadata for the report
+        project: "Playwright Project",
+        version: "1.0.0",
+        description: "Playwright test report for my application",
+        release: "1.0",
+        platform: "Windows", // or os.type() for dynamic platform
+      },
+    };
+
+  
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './tests-examples',
+  /* Run tests in files in parallel */
+  fullyParallel: false,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [
+    ['html'],
+    ['ortoni-report', reportConfig]
+  ],
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://localhost:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+     headless: false,
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+/*
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+});
